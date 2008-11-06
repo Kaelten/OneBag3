7aaf85187209179f81e301e62ac1ab7635463b5c
Bryan McLemore <kaelten@gmail.com>
2008-11-05 22:50:13 -0600

    start of new config screen, oh ya, and Ackis is just jealous of my unused murky code.

--- OneConfig3.lua
+++ OneConfig3.lua
@@ -5,6 +5,7 @@ local AceConfigDialog   = LibStub("AceConfigDialog-3.0")
 local OneCore3 = LibStub('AceAddon-3.0'):GetAddon('OneCore3')
 local ModulePrototype = OneCore3.defaultModulePrototype
 
+local L = LibStub("AceLocale-3.0"):GetLocale("OneCore3")
 
 function ModulePrototype:InitializeConfiguration()
 	self.configs = {}
@@ -13,14 +14,40 @@ function ModulePrototype:InitializeConfiguration()
 		return {
 			type = "group",
 			name = self.displayName,
-			args = {}
+			args = {
+				general = {
+					type = "group",
+					name = self.displayName,
+					args = {
+						desc = {
+							type = "description",
+							name = L["Description"]:format(self.displayName),
+							order = 1,
+						},
+						cols = {
+							order = 20, 
+							type = "range",
+							name = L["Number of Columns"],
+							desc = "Sets the maximum number of columns to use",
+							min = 1, max = 30, step = 1,
+							get = function(info)
+								return self.db.profile.appearance.cols
+							end,
+							set = function(info, cols)
+								self.db.profile.appearance.cols = cols
+								self:OrganizeFrame(true)
+							end	
+						},
+					}
+				}
+			}
 		}
 	end
 	
 	baseconfig = GetBaseConfig()
 	
 	AceConfig:RegisterOptionsTable(self.displayName, baseconfig)
-	self.configs.main = AceConfigDialog:AddToBlizOptions(self.displayName)
+	self.configs.main = AceConfigDialog:AddToBlizOptions(self.displayName, nil, nil, 'general')
 end
 
 function ModulePrototype:OpenConfig()
---
+++ localizations/enUS.lua
@@ -0,0 +1,7 @@
+-- Please make sure to save the file as UTF-8; ¶
+--
+local L = LibStub("AceLocale-3.0"):NewLocale("OneCore3","enUS", true)
+
+-- Basic Config
+L["Number of Columns"] = true
+L["Description"] = "%s collapses all of your bag slots into one frame."
