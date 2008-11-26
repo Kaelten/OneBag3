03820fe9149f9ef9043b2e1028f907800c24ed93
Bryan McLemore <kaelten@gmail.com>
2008-11-26 00:35:37 -0600

    moved a lot of code into the core, cleaned up some hook handlers, fixed a few bugs

--- OneBag3.lua
+++ OneBag3.lua
@@ -28,7 +28,6 @@ function OneBag3:OnInitialize()
 
 		self:BuildFrame()
 		self:OrganizeFrame()
-		
 		self:UpdateFrame()
 		
 		local UpdateBag = function(event, bag) 
@@ -162,14 +161,10 @@ end
 -- Hooks handlers
 function OneBag3:IsBagOpen(bag)
 	if type(bag) == "number" and (bag < 0 or bag > 4) then
-		return 
+		return self.hooks.IsBagOpen(bag)
 	end
 	
-	if self.frame:IsVisible() then
-		return bag
-	else
-		return nil	
-	end
+	return self.isOpened and bag or nil
 end
 
 function OneBag3:ToggleBag(bag)
@@ -204,6 +199,7 @@ function OneBag3:CloseBag(bag)
 	self.isOpened = false
 end
 
+--Custom Button Getter
 function OneBag3:GetBackbackButton(parent)
 	local button = CreateFrame("CheckButton", "OBSideBarBackpackButton", parent, "ItemButtonTemplate")
 	button:SetID(0)
@@ -240,42 +236,7 @@ function OneBag3:GetBackbackButton(parent)
 	
 	button:SetScript("OnReceiveDrag", function(event, btn) BackpackButton_OnClick(button, btn) end)
 	
-	return button
-	
-end
-
-function OneBag3:GetBagButton(bag, parent)
-	local button = CreateFrame("CheckButton", "OBSideBarBag"..bag.."Slot", parent, "BagSlotButtonTemplate")
-	
-	button:SetScale(1.27)
-	
-	self:SecureHookScript(button, "OnEnter", function(button)
-		self:HighlightBagSlots(button:GetID()-19)
-	end)
-	
-	button:SetScript("OnLeave", function(button)
-		if not button:GetChecked() then
-			self:UnhighlightBagSlots(button:GetID()-19)
-			self.frame.bags[button:GetID()-19].colorLocked = false
-		else
-			self.frame.bags[button:GetID()-19].colorLocked = true
-		end
-		GameTooltip:Hide()
-	end)
-	
-	button:SetScript("OnClick", function(button) 
-		local haditem = PutItemInBag(button:GetID())
-
-		if haditem then
-			button:SetChecked(not button:GetChecked())
-		end 
-	end)
-	
-	button:SetScript("OnReceiveDrag", function(button) 
-		PutItemInBag(button:GetID())
-	end)
-	
-	return button
+	return button	
 end
 
 function OneBag3:GetButtonOrder()
@@ -309,106 +270,4 @@ function OneBag3:GetButtonOrder()
 	end
 	
 	return slots
-end
-
-function OneBag3:BuildFrame()
-	for _, bag in pairs(self.bagIndexes) do
-		local size = GetContainerNumSlots(bag)
-		local bagType = select(2, GetContainerNumFreeSlots(bag))
-		
-		if not self.frame.bags then
-			self.frame.bags = {}
-		end
-		
-		if not self.frame.bags[bag] then
-			self.frame.bags[bag] = self:GetBag(self.frame, bag)
-		end		
-		
-		self.frame.bags[bag].type = bagType
-		
-		if self.frame.bags[bag].size ~= size then
-			self.frame.bags[bag].size = size
-			self.doOrganization = true
-		end
-		
-		for slot = 1, size do
-			slotkey = ('%s:%s'):format(bag, slot)
-			if not self.frame.slots[slotkey] then
-				self.frame.slots[slotkey] = self:GetButton(self.frame.bags[bag], slot)
-				self.frame.slots[slotkey]:SetFrameStrata(self.stratas[self.db.profile.behavior.strata])
-				self.doOrganization = true
-			end
-		end
-	end
-end
-
-function OneBag3:OrganizeFrame(force)
-	if (not self.doOrganization and not force) or not self.frame.slots then
-		return 
-	end
-	
-	local cols, curCol, curRow, maxCol, justinc = self.db.profile.appearance.cols, 1, 1, 0, false
-	
-	for slotkey, slot in pairs(self.frame.slots) do
-		slot:Hide()
-	end
-	
-	for slotkey, slot in pairs(self:GetButtonOrder()) do
-		if type(slot) == 'string' or slot:ShouldShow() then
-			if slot.ClearAllPoints then
-				justinc = false
-				slot:ClearAllPoints()
-				slot:SetPoint("TOPLEFT", self.frame:GetName(), "TOPLEFT", self.leftBorder + self.colWidth * (curCol - 1), 0 - self.topBorder - (self.rowHeight * curRow))
-				slot:SetFrameLevel(self.frame:GetFrameLevel()+20)
-				slot:Show()
-				curCol = curCol + 1
-			end
-		
-			if slot == "BLANK" then
-				curCol = curCol + 1
-			end
-		
-			maxCol = math.max(maxCol, curCol-1)
-		
-			if (curCol > cols or slot == "NEWLINE") and not justinc then
-				curCol, curRow, justinc = 1, curRow + 1, true
-			end
-		end
-	end
-	
-	if not justinc then curRow = curRow + 1 end
-	self.frame:SetHeight(curRow * self.rowHeight + self.bottomBorder + self.topBorder) 
-	self.frame:SetWidth(maxCol * self.colWidth + self.leftBorder + self.rightBorder)
-	
-	self.doOrganization = false
-end
-
-function OneBag3:UpdateBag(bag)
-	if not self.frame.bags[bag] then
-		return
-	end
-	
-	self:BuildFrame()
-	self:OrganizeFrame()
-	
-	if not self.frame.bags[bag].colorLocked then
-		for slot=1, self.frame.bags[bag].size do
-			self:ColorBorder(self:GetSlot(bag, slot))
-		end
-	end
-	
-	if self.frame.bags[bag].size and self.frame.bags[bag].size > 0 then
-		ContainerFrame_Update(self.frame.bags[bag])
-	end
-end
-
-function OneBag3:GetSlot(bagid, slotid)
-	key = ('%s:%s'):format(bagid, slotid)
-	return self.frame.slots[key]
-end
-
-function OneBag3:UpdateFrame()
-	for _, bag in pairs(self.bagIndexes) do
-		self:UpdateBag(bag)
-	end
 end
--- OneCore3.lua
+++ OneCore3.lua
@@ -218,6 +218,40 @@ function ModulePrototype:UnhighlightBagSlots(bagid)
 	end
 end
 
+function ModulePrototype:GetBagButton(bag, parent)
+	local button = CreateFrame("CheckButton", "OBSideBarBag"..bag.."Slot", parent, "BagSlotButtonTemplate")
+	
+	button:SetScale(1.27)
+	
+	self:SecureHookScript(button, "OnEnter", function(button)
+		self:HighlightBagSlots(button:GetID()-19)
+	end)
+	
+	button:SetScript("OnLeave", function(button)
+		if not button:GetChecked() then
+			self:UnhighlightBagSlots(button:GetID()-19)
+			self.frame.bags[button:GetID()-19].colorLocked = false
+		else
+			self.frame.bags[button:GetID()-19].colorLocked = true
+		end
+		GameTooltip:Hide()
+	end)
+	
+	button:SetScript("OnClick", function(button) 
+		local haditem = PutItemInBag(button:GetID())
+
+		if haditem then
+			button:SetChecked(not button:GetChecked())
+		end 
+	end)
+	
+	button:SetScript("OnReceiveDrag", function(button) 
+		PutItemInBag(button:GetID())
+	end)
+	
+	return button
+end
+
 function ModulePrototype:GetBag(parent, id)
 	local bag = CreateFrame("Frame", parent:GetName().."Bag"..id, parent)
 	bag:SetID(id)
@@ -249,6 +283,108 @@ function ModulePrototype:GetButton(parent, id)
 	end
 	
 	return button
+end
+
+function ModulePrototype:BuildFrame()
+	for _, bag in pairs(self.bagIndexes) do
+		local size = GetContainerNumSlots(bag)
+		local bagType = select(2, GetContainerNumFreeSlots(bag))
+		
+		if not self.frame.bags then
+			self.frame.bags = {}
+		end
+		
+		if not self.frame.bags[bag] then
+			self.frame.bags[bag] = self:GetBag(self.frame, bag)
+		end		
+		
+		self.frame.bags[bag].type = bagType
+		
+		if self.frame.bags[bag].size ~= size then
+			self.frame.bags[bag].size = size
+			self.doOrganization = true
+		end
+		
+		for slot = 1, size do
+			slotkey = ('%s:%s'):format(bag, slot)
+			if not self.frame.slots[slotkey] then
+				self.frame.slots[slotkey] = self:GetButton(self.frame.bags[bag], slot)
+				self.frame.slots[slotkey]:SetFrameStrata(self.stratas[self.db.profile.behavior.strata])
+				self.doOrganization = true
+			end
+		end
+	end
+end
+
+function ModulePrototype:OrganizeFrame(force)
+	if (not self.doOrganization and not force) or not self.frame.slots then
+		return 
+	end
+	
+	local cols, curCol, curRow, maxCol, justinc = self.db.profile.appearance.cols, 1, 1, 0, false
+	
+	for slotkey, slot in pairs(self.frame.slots) do
+		slot:Hide()
+	end
+	
+	for slotkey, slot in pairs(self:GetButtonOrder()) do
+		if type(slot) == 'string' or slot:ShouldShow() then
+			if slot.ClearAllPoints then
+				justinc = false
+				slot:ClearAllPoints()
+				slot:SetPoint("TOPLEFT", self.frame:GetName(), "TOPLEFT", self.leftBorder + self.colWidth * (curCol - 1), 0 - self.topBorder - (self.rowHeight * curRow))
+				slot:SetFrameLevel(self.frame:GetFrameLevel()+20)
+				slot:Show()
+				curCol = curCol + 1
+			end
+		
+			if slot == "BLANK" then
+				curCol = curCol + 1
+			end
+		
+			maxCol = math.max(maxCol, curCol-1)
+		
+			if (curCol > cols or slot == "NEWLINE") and not justinc then
+				curCol, curRow, justinc = 1, curRow + 1, true
+			end
+		end
+	end
+	
+	if not justinc then curRow = curRow + 1 end
+	self.frame:SetHeight(curRow * self.rowHeight + self.bottomBorder + self.topBorder) 
+	self.frame:SetWidth(maxCol * self.colWidth + self.leftBorder + self.rightBorder)
+	
+	self.doOrganization = false
+end
+
+function ModulePrototype:UpdateBag(bag)
+	if not self.frame.bags[bag] then
+		return
+	end
+	
+	self:BuildFrame()
+	self:OrganizeFrame()
+	
+	if not self.frame.bags[bag].colorLocked then
+		for slot=1, self.frame.bags[bag].size do
+			self:ColorBorder(self:GetSlot(bag, slot))
+		end
+	end
+	
+	if self.frame.bags[bag].size and self.frame.bags[bag].size > 0 then
+		ContainerFrame_Update(self.frame.bags[bag])
+	end
+end
+
+function ModulePrototype:GetSlot(bagid, slotid)
+	key = ('%s:%s'):format(bagid, slotid)
+	return self.frame.slots[key]
+end
+
+function ModulePrototype:UpdateFrame()
+	for _, bag in pairs(self.bagIndexes) do
+		self:UpdateBag(bag)
+	end
 end
 
 
