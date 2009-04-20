7ab62e28657525f29ced76cd592ad980d506d72a
Bryan McLemore <kaelten@gmail.com>
2009-04-20 02:58:28 -0500

    fix broken zhTW localization

--- localization/zhTW.lua
+++ localization/zhTW.lua
@@ -1,7 +1,7 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("OneBag3", "deDE")
+local L = LibStub("AceLocale-3.0"):NewLocale("OneBag3", "zhTW")
 
 if L then 
     --[===[@non-debug@
-    @localization(locale="deDE", format="lua_additive_table")@
+    @localization(locale="zhTW", format="lua_additive_table")@
     --@end-non-debug@]===]
 end
