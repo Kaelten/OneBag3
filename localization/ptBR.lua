7a5cea9b103100a9cbce2e3bfe80d72ffc0137dc
Bryan McLemore <kaelten@gmail.com>
2014-10-18 19:05:43 -0500

    Fixing up for missing locales

---
+++ localization/ptBR.lua
@@ -0,0 +1,7 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("OneBag3", "ptBR")
+
+if L then
+    --[===[@non-debug@
+    @localization(locale="ptBR", format="lua_additive_table")@
+    --@end-non-debug@]===]
+end
